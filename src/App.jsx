fc14785a31427c0ea047eac07eb0bbf359f5c460
unknown <braz.souza@acct.global>
2024-03-30 14:36:10 -0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './styles/globals.scss';
+
+function App() {
+	return (
+		<>
+			<div className="container">
+				<h1>Welcome to our website!</h1>
+			</div>
+		</>
+	);
+}
+
+export default App;
